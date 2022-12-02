af8fa3946b2941c6147cb3bc57d88481ea46ccd9
Petros Amiridis <petros@amiridis.net>
2022-12-02 19:48:44 +0200

    Start solving 01a
    - Open the puzzle input and read it into a vector of strings
    - Print the contents of the vector on screen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Solving puzzle 01a...");
+    solve_puzzle_01a();
+}
+
+// Open the text file and read it into a vector of strings — DONE
+// Print the contents — DONE
+// Create a vector of integers that hols the sums of each Elf
+// Find the largerst number in that vector
+fn solve_puzzle_01a() {
+    let calories: Vec<String> = load_from_file("src/01a-puzzle-input.txt");
+    for (pos, e) in calories.iter().enumerate() {
+        println!("{}: {:?}", pos, e)
+    }
+}
+
+fn load_from_file(file_path: &str) -> Vec<String> {
+    let file = File::open(file_path).expect("file wasn't found.");
+    let reader = BufReader::new(file);
+
+    let numbers: Vec<String> = reader
+        .lines()
+        .map(|line| line.unwrap().parse::<String>().unwrap())
+        .collect();
+    return numbers;
 }
