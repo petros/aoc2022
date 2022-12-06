2aa7ea42653741eecd2f389fcb5546025359c702
Petros Amiridis <petros@amiridis.net>
2022-12-06 20:00:09 +0200

    Solve part two of day one
    Find the total calories of the top three most calory carrying Elfs.

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,31 @@ use std::io::BufRead;
 use std::io::BufReader;
 
 fn main() {
-    println!("Solving puzzle 01a...");
-    solve_puzzle_01a();
+    solve_day_01();
 }
 
-// Open the text file and read it into a vector of strings — DONE
-// Print the contents — DONE
-// Create a vector of integers that holds the sums of each Elf — DONE
-// Find the largerst number in that vector
-fn solve_puzzle_01a() {
+fn solve_day_01() {
+    println!("--- Day 1: Calorie Counting ---");
     let calories: Vec<String> = load_from_file("src/01a-puzzle-input.txt");
     let mut group_sum: u32 = 0;
     let mut elf_calories: u32;
     let mut elf_calory_sums: Vec<u32> = Vec::new();
-    for (pos, e) in calories.iter().enumerate() {
-        println!("{}: {:?}", pos, e);
+    for (_pos, e) in calories.iter().enumerate() {
         if e == "" {
             elf_calory_sums.push(group_sum);
-            println!("Sum {group_sum}");
             group_sum = 0;
         } else {
             elf_calories = e.parse::<u32>().unwrap();
             group_sum += elf_calories;
         }
     }
+    let max_total_calories: &u32 = elf_calory_sums.iter().max().unwrap();
+    println!("The total calories of the Elf carrying the most calories are {max_total_calories}");
+    println!("--- Part Two ---");
+    elf_calory_sums.sort_by(|a, b| b.cmp(a));
+    let total: u32 = elf_calory_sums[0] + elf_calory_sums[1] + elf_calory_sums[2];
+    println!("The top three most calories carrying Elfs have a total of {total} calories");
+    println!();
 }
 
 fn load_from_file(file_path: &str) -> Vec<String> {
