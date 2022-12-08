4f0184de4aec881925046616c194560b1f134968
Petros Amiridis <petros@amiridis.net>
2022-12-08 18:01:57 +0200

    Get example input

--- src/day04.rs
+++ src/day04.rs
@@ -7,6 +7,17 @@ pub fn solve_day() {
     test_example_p2();
 }
 
+fn get_example_input() -> Vec<String> {
+    vec![
+        String::from("2-4,6-8"),
+        String::from("2-3,4-5"),
+        String::from("5-7,7-9"),
+        String::from("2-8,3-7"),
+        String::from("6-6,4-6"),
+        String::from("2-6,4-8"),
+    ]
+}
+
 fn test_example() {}
 
 fn test_example_p2() {}
