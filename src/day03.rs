7f5dfe2acff48e4955cda37f3a7529c6ad97c0a9
Petros Amiridis <petros@amiridis.net>
2022-12-08 09:34:50 +0200

    Prepare day three

---
+++ src/day03.rs
@@ -0,0 +1,11 @@
+pub fn solve_day_03() {
+    println!("--- Day 3: Rucksack Reorganization ---");
+    test_example();
+    println!("  --- Part Two ---");
+    println!();
+    test_example_p2();
+}
+
+fn test_example() {}
+
+fn test_example_p2() {}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod day01;
 mod day02;
+mod day03;
 mod utils;
 
 fn main() {
     day01::solve_day_01();
     day02::solve_day_02();
+    day03::solve_day_03();
 }
