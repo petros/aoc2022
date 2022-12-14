317821c6a2878a74b2cd325163adeb4c64ef5410
Petros Amiridis <petros@amiridis.net>
2022-12-14 18:18:56 +0200

    Solve day four part two

--- src/day04.rs
+++ src/day04.rs
@@ -1,5 +1,5 @@
 use crate::utils::load_from_file;
-//use std::iter::Filter;
+use std::collections::HashSet;
 
 // https://adventofcode.com/2022/day/4
 pub fn solve_day() {
@@ -22,10 +22,31 @@ pub fn solve_day() {
         })
         .collect::<Vec<_>>()
         .len();
-    println!("There are {count} assignment pairs where one range fully contain the other.");
+    println!("  There are {count} assignment pairs where one range fully contain the other.");
     println!("  --- Part Two ---");
-    println!();
     test_example_p2();
+    let count = load_from_file("src/puzzle_inputs/day04.txt")
+        .iter()
+        .filter(|line| {
+            let splits: Vec<&str> = line.split(",").collect();
+            let left = splits[0];
+            let right = splits[1];
+            let left_splits: Vec<&str> = left.split("-").collect();
+            let right_splits: Vec<&str> = right.split("-").collect();
+            let left_start: usize = left_splits[0].parse::<usize>().unwrap_or(0);
+            let left_end: usize = left_splits[1].parse::<usize>().unwrap_or(0);
+            let right_start: usize = right_splits[0].parse::<usize>().unwrap_or(0);
+            let right_end: usize = right_splits[1].parse::<usize>().unwrap_or(0);
+            let list_a: Vec<_> = (left_start..=left_end).collect();
+            let list_b: Vec<_> = (right_start..=right_end).collect();
+            let a: HashSet<_> = list_a.into_iter().collect();
+            let b: HashSet<_> = list_b.into_iter().collect();
+            !a.intersection(&b).collect::<Vec<_>>().is_empty()
+        })
+        .collect::<Vec<_>>()
+        .len();
+    println!("  The ranges overlap in {count} assignment pairs.");
+    println!();
 }
 
 fn get_example_input() -> Vec<String> {
@@ -60,4 +81,26 @@ fn test_example() {
     assert_eq!(count, 2);
 }
 
-fn test_example_p2() {}
+fn test_example_p2() {
+    let count = get_example_input()
+        .iter()
+        .filter(|line| {
+            let splits: Vec<&str> = line.split(",").collect();
+            let left = splits[0];
+            let right = splits[1];
+            let left_splits: Vec<&str> = left.split("-").collect();
+            let right_splits: Vec<&str> = right.split("-").collect();
+            let left_start: usize = left_splits[0].parse::<usize>().unwrap_or(0);
+            let left_end: usize = left_splits[1].parse::<usize>().unwrap_or(0);
+            let right_start: usize = right_splits[0].parse::<usize>().unwrap_or(0);
+            let right_end: usize = right_splits[1].parse::<usize>().unwrap_or(0);
+            let list_a: Vec<_> = (left_start..=left_end).collect();
+            let list_b: Vec<_> = (right_start..=right_end).collect();
+            let a: HashSet<_> = list_a.into_iter().collect();
+            let b: HashSet<_> = list_b.into_iter().collect();
+            !a.intersection(&b).collect::<Vec<_>>().is_empty()
+        })
+        .collect::<Vec<_>>()
+        .len();
+    assert_eq!(count, 4);
+}
